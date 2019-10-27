95fa6cffae41860b8a3f0bf35a4d744234580f4c
Bruno Sabença <bruno@evesleep.co.uk>
2019-10-27 13:44:43 +0000

    Restructures files and adds clock

---
+++ resources/js/time.js
@@ -0,0 +1,8 @@
+function setTime() {
+   const $time = document.querySelector('#time');
+   const time = new Date();
+   time.innerText = time.toLocaleTimeString();
+   setTimeout(function () {
+    setTime();
+}, 1000);
+}
